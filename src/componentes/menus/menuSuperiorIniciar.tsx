e9cf2b67de53d339439824e15c08a33d03476168
Gustavo Rocha <87160095+MyNameIsGustavo@users.noreply.github.com>
2025-07-11 16:39:51 -0300

    alterações

--- src/componentes/menus/menuSuperiorIniciar.tsx
+++ src/componentes/menus/menuSuperiorIniciar.tsx
@@ -4,21 +4,26 @@ import Navbar from 'react-bootstrap/Navbar';
 
 export default function MenuSuperiorIniciar() {
   return (
-    <Navbar expand="md" style={{ height: '100px' }}>
+    <Navbar expand="lg" style={{ height: '100px' }}>
       <Container className="justify-content-between align-items-center">
         <Navbar.Brand href="#home">
           <img
             src="/images/alberflex.png"
             alt="Alberflex"
             height="50"
-            className="d-inline-block align-top"
+            
           />
         </Navbar.Brand>
-        <Nav>
-          <Nav.Link href="#home" className="text-gray fs-5 fw-bold">
-            CENTRAL ALBERFLEX
-          </Nav.Link>
-        </Nav>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home" className="text-gray fs-5 fw-bold">
+              Central Alberflex.
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
--- src/telas/paginaInicial/paginaInical.tsx
+++ src/telas/paginaInicial/paginaInical.tsx
@@ -1,7 +1,20 @@
+import React from "react";
 import MenuSuperiorIniciar from "../../componentes/menus/menuSuperiorIniciar";
 
 export default function PaginaInicial() {
   return (
-    <MenuSuperiorIniciar />
+    <>
+      <MenuSuperiorIniciar />
+      <img
+        src="/images/Capa.png"
+        alt="Capa"
+        style={{
+          width: "100%",
+          height: "65vh",  
+          objectFit: "cover",
+          display: "block",
+        }}
+      />
+    </>
   );
 }
